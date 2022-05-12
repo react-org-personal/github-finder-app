67af5069eb17c53baad76c84a9a4b0a4f1f5764b
Anonav0 <commonpeople27@gmail.com>
2022-05-12 21:13:11 +0530

    Setup Github context

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Notfound from './pages/Notfound';
-import './App.css';
-import Footer from './components/layout/Footer';
-import Navbar from './components/layout/Navbar';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Notfound from "./pages/Notfound";
+import "./App.css";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import { GithubProvider } from "./context/github/github-context";
 
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar/>
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/about' element={<About/>} />
-            <Route path='/notfound' element={<Notfound/>} />
-            <Route path='/*' element={<Notfound/>} />
-          </Routes>
-        </main>
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<Notfound />} />
+              <Route path="/*" element={<Notfound />} />
+            </Routes>
+          </main>
 
-        <Footer/>
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
--- src/components/users/UserList.jsx
+++ src/components/users/UserList.jsx
@@ -1,25 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
+import GithubContext from "../../context/github/github-context";
+
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const {users, isLoading, fetchResults} = useContext(GithubContext);
+
   useEffect(() => {
     fetchResults();
   }, []);
 
-  const fetchResults = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_KEY}`,
-      },
-    });
-    const data = await response.json();
 
-    setUsers(data);
-    setIsLoading(false);
-  };
+
+
 
   const userData = users.map((user) => <UserItem user={user}/>);
 
